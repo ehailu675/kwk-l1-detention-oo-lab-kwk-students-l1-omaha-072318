bcc42f89c1e3545b5175dcef57f61092c4cf9aa4
ehailu675 <elisabethahailu@gmail.com>
2018-07-30 15:07:07 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,41 @@
 #Code your detention class here
+class Detention
+  
+  def location=(location)
+ @location = location
+ end
+ 
+ def location
+   @location
+ end
+  
+  def time=(time)
+    @time = time
+  end
+ 
+  def time
+    @time
+  end
+ 
+  def grumpy_teacher=(grumpy_teacher)
+    @grumpy_teacher = grumpy_teacher
+  end
+  
+  def grumpy_teacher
+    @grumpy_teacher
+  end
+  
+  def activity=(activity)
+    @activity = activity
+  end
+  
+  def activity
+    @activity
+  end
+   
+   
+    
+  
+end
+
+  
